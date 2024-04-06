b171228d9d1fa0efc46e0606949b04baa872f2df
Bipin Alchetti <30284935+bipainalchetti@users.noreply.github.com>
2024-04-06 21:26:41 +0000

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Configure the Azure Provider
 terraform {
   required_providers {
     azurerm = {
@@ -16,69 +17,70 @@ terraform {
       key                  = "terraform.tfstate"
   }  
 }
-
-provider "azurerm" {
-  # Configuration options
-  features {}
+# Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "ODL-azure-1288220"
+  location = "eastus"
 }
 
-data "azurerm_resource_group" "example" {
-  name = ODL-azure-1288206
-  
-}
+# Create a Key Vault
+resource "azurerm_key_vault" "keyvault" {
+  name                        = "odlkeyvault1288220"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
 
-resource "random_string" "random_string"{
-    length = 6
-    lower = true
-    upper = false
-    number = true
-    special = false
-}
+  sku_name = "standard"
 
-locals {
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-    keyvault_key_name = substr(lower(join("",
-    ["kys",
-    substr(next,0,1),
-    substr(sid1, 0, 4),
-    local.keyvault_name,
-    random_string.random_string.result
-    ])), 0, 15)
-}
+    key_permissions = [
+      "get", "list", "create", "delete", "update",
+    ]
 
+    secret_permissions = [
+      "get", "list", "set", "delete",
+    ]
 
-# Create an Azure Key Vault
-resource "azurerm_key_vault" "example" {
-  name                = "example-keyvault"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-}
-
-# Create a customer-managed key in the Key Vault
-resource "azurerm_key_vault_key" "example" {
-  name         = local.keyvault_key_name
-  key_vault_id = azurerm_key_vault.example.id
-  key_type     = "RSA"
-  key_size     = 2048
-  key_opts     = ["decrypt", "encrypt", "sign", "verify"]
-}
-
-# Create an Azure Storage Account
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorage"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  # Attach the customer-managed key to the Storage Account
-  customer_managed_key {
-    key_vault_key_id = azurerm_key_vault_key.example.id
+    storage_permissions = [
+      "get", "list",
+    ]
   }
 }
 
+# Create a Key in the Key Vault
+resource "azurerm_key_vault_key" "key" {
+  name         = "storage-account-key"
+  key_vault_id = azurerm_key_vault.keyvault.id
+  key_type     = "RSA"
+  key_size     = 2048
 
-# Retrieve the current client configuration
-data "azurerm_client_config" "current" {}
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+}
+
+# Create a Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = "odlstorage1288220"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  # Associate the Key from Key Vault with the Storage Account
+  customer_managed_key {
+    key_vault_key_id = azurerm_key_vault_key.key.id
+    key_name         = azurerm_key_vault_key.key.name
+  }
+}
