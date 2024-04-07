0c75d29f26ab4b406823c086db4d5fd1e6a02d61
Bipin Alchetti <30284935+bipainalchetti@users.noreply.github.com>
2024-04-07 18:35:18 +0000

    new env

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_user_assigned_identity" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorage1288824"
+  name                     = "examplestorage12888245"
   resource_group_name      = data.azurerm_resource_group.example.name
   location                 = data.azurerm_resource_group.example.location
   account_tier             = "Standard"
@@ -76,19 +76,6 @@ resource "azurerm_key_vault_key" "example" {
   key_type     = "RSA"
   key_size     = 2048
   key_opts     = ["unwrapKey", "wrapKey"]
-}
-
-resource "azurerm_role_assignment" "example" {
-  scope              = data.azurerm_resource_group.example.id
-  role_definition_name = "Reader"
-  principal_id       = azurerm_user_assigned_identity.example.principal_id
-}
-
-resource "azurerm_storage_account_customer_managed_key" "example" {
-  storage_account_id = azurerm_storage_account.example.id
-  key_vault_id       = azurerm_key_vault.example.id
-  key_name           = azurerm_key_vault_key.example.name
-  user_assigned_identity_id = azurerm_user_assigned_identity.example.id
 }
 
 
