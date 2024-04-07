c1a3ad71e3ef53b644816f72e6f28208b4a5fca1
Bipin Alchetti <30284935+bipainalchetti@users.noreply.github.com>
2024-04-07 18:07:21 +0000

    new env

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "ODL-azure-1288488"
+      resource_group_name  = "ODL-azure-1288824"
       storage_account_name = "tfstate14339"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
@@ -23,72 +23,63 @@ provider "azurerm" {
 }
 
 # Create a Resource Group
-data "azurerm_resource_group" "rg" {
-  name     = "ODL-azure-1288488"
+data "azurerm_resource_group" "example" {
+  name     = "ODL-azure-1288824"
 }
 
-# Create a Key Vault
-resource "azurerm_key_vault" "keyvault" {
-  name                        = "odlkeyvault1288488"
-  location                    = data.azurerm_resource_group.rg.location
-  resource_group_name         = data.azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+resource "azurerm_user_assigned_identity" "example" {
+  name                = "example-identity"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+}
 
-  sku_name = "standard"
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorage1288824"
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.example.id]
+  }
+}
+
+resource "azurerm_key_vault" "example" {
+  name                = "examplekeyvault1288824"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      "Get", "List", "Create", "Delete", "Update", "GetRotationPolicy", "SetRotationPolicy"
-    ]
-
-    secret_permissions = [
-      "Get", "List", "Set", "Delete",
-    ]
-
-    storage_permissions = [
-      "Get", "List",
+      "Get",
+      "UnwrapKey",
+      "WrapKey",
+      "GetRotationPolicy",
+      "SetRotationPolicy"
     ]
   }
 }
 
-# Create a Key in the Key Vault
-resource "azurerm_key_vault_key" "key" {
-  name         = "storage-account-key"
-  key_vault_id = azurerm_key_vault.keyvault.id
+resource "azurerm_key_vault_key" "example" {
+  name         = "examplekey1288824"
+  key_vault_id = azurerm_key_vault.example.id
   key_type     = "RSA"
   key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-}
-
-# Create a Storage Account
-resource "azurerm_storage_account" "storage" {
-  name                     = "odlstorage1288488"
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  # Associate the Key from Key Vault with the Storage Account
+  key_opts     = ["unwrapKey", "wrapKey"]
 }
 
 resource "azurerm_storage_account_customer_managed_key" "example" {
-  storage_account_id = azurerm_storage_account.storage.id
-  key_vault_id       = azurerm_key_vault.keyvault.id
-  key_name           = azurerm_key_vault_key.key.name
+  storage_account_id = azurerm_storage_account.example.id
+  key_vault_id       = azurerm_key_vault.example.id
+  key_name           = azurerm_key_vault_key.example.name
+  user_assigned_identity_id = azurerm_user_assigned_identity.example.id
 }
 
 # Retrieve the current client configuration
